63dfd2ab3d8a16a2067c80d6b794761f7ad95360
himarr <denizhimar@gmail.com>
2023-10-19 13:55:09 +0100

    a

---
+++ src/main/java/org/example/SerializationExercises.java
@@ -0,0 +1,40 @@
+package org.example;
+
+//import com.google.gson.Gson;
+import java.io.*;
+
+public class SerializationExercises {
+    /*
+        Should define the class for the concepts Movie, Theater and Session.
+        A session is a play of movie in a theater.
+        Create 2 instances of each class and relate among them.
+        Serialize to Json all objects and save then in different files.
+     */
+    public static class Exercise1 {
+        public static void main(String[] args) {
+
+        }
+    }
+
+    /*
+        Deserialize the objects created in exercise 1.
+        Now serialize them using ObjectOutputStream
+     */
+    public static class Exercise2 {
+
+        public static void main(String[] args) {
+
+        }
+    }
+
+    /*
+       Deserialize the objects from the binary files created in exercise 2.
+    */
+    public static class Exercise3 {
+
+        public static void main(String[] args) {
+
+        }
+    }
+}
+
